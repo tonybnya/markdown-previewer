8429be1da3463a2679d645fa793968f4702c85c6
tonybnya <nya.tony2010@gmail.com>
2024-05-16 21:31:24 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/css/App.css";
 import Editor from "./components/Editor";
 import Preview from "./components/Preview";
 
 const App = () => {
+  const defaultText = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '"""' && lastLine == '"""') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course, there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
+
+  const [text, setText] = useState(defaultText);
+
   return (
     <div id="app">
       <h1 id="title">Markdown Previewer</h1>
       <div id="container">
-        <Editor />
-        <Preview />
+        <Editor content={text} setContent={setText} />
+        <Preview content={text} />
       </div>
     </div>
   );
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "../styles/css/styles.css";
 import Header from "./Header";
-import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
+import ReactMarkdown from "react-markdown";
 
 const Preview = ({ content }) => {
   return (
     <div id="preview-container">
       <Header text="Previewer" />
-      <div id="preview">{content}</div>
-      {/* <div id="preview">{marked.parse(content)}</div> */}
+      <div id="preview">
+        <ReactMarkdown>{content}</ReactMarkdown>
+      </div>
     </div>
   );
 };
