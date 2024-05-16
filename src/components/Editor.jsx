14b9a7bd5e1ef7c29468965f0829dbb153706ef1
tonybnya <nya.tony2010@gmail.com>
2024-05-16 13:37:29 +0100

    built: install Marked library

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import "../styles/css/styles.css";
 import Header from "./Header";
-import defaultText from "../data/default.txt";
 
 const Editor = ({ content, setContent }) => {
   return (
     <div id="editor-container">
       <Header text="Editor" />
-      <textarea id="editor" value={defaultText}></textarea>
+      <textarea
+        id="editor"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      ></textarea>
     </div>
   );
 };
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "../styles/css/styles.css";
 import Header from "./Header";
+import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
 
-const Preview = () => {
+const Preview = ({ content }) => {
   return (
     <div id="preview-container">
       <Header text="Previewer" />
-      <div id="preview">Preview</div>
+      <div id="preview">{marked.parse(content)}</div>
     </div>
   );
 };
